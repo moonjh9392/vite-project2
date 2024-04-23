ccf57bfc20b1aa7f63312c2a14033a1d8c1e643d
moonjh9392 <moonjh9392@gmail.com>
2024-04-23 16:35:06 +0900

    ssr 제거

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-// );
-
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-
-if (container.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    container,
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-} else {
-  root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
